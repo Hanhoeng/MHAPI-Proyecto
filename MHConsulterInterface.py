677606d232d0789e83909e22684d0df5ccb20c28
Hiroshi <sephirothMH@outlook.com>
2021-04-19 23:14:33 -0500

    Sepracion de archivo MHConsulter en MHConsulterInterface y MHRecorder

---
+++ MHConsulterInterface.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+
+# Entidades
+## Monstruos
+class Monstruo():
+    def __init__(self, nombre:str,debilidades:list,resistencia:str,descripcion:str):
+        self.nombre = nombre
+        self.debilidades = debilidades
+        self.resistencia = resistencia
+        self.descripcion = descripcion
+
+    def __str__(self) -> str:
+        self.cadena = "Nombre: {}\n".format(self.nombre)
+        self.cadena += "Debilidades: {}\n".format(self.debilidades)
+        self.cadena += "resistencia: {}\n".format(self.resistencia)
+        self.cadena += "descripción: {}\n".format(self.descripcion)
+        return self.cadena
+
+# Interfaces
+class MHConsulter(ABC):
+
+    @abstractmethod
+    def getMonsterByName(self, nombre:str) -> Monstruo:
+        pass
+
+    @abstractmethod
+    def getMonsterByDebilidad(self, debilidad:str) -> Monstruo:
+        pass
+
+    @abstractmethod
+    def getMonsterByResistencia(self, resistencia:str) -> Monstruo:
+        pass
--- MHRecorder.py
+++ MHRecorder.py
@@ -1,7 +1,4 @@
 from abc import ABC, abstractmethod
-from pymongo import MongoClient
-import requests as peticion
-import json
 
 # Entidades
 ## Monstruos
@@ -18,31 +15,12 @@ class Monstruo():
         self.cadena += "resistencia: {}\n".format(self.resistencia)
         self.cadena += "descripción: {}\n".format(self.descripcion)
         return self.cadena
-
-# Interfaces
-
-class MHConnector(ABC):
-    host = "localhost"
-    port = 27017
-    MongoClient(host,port)
-
-class MHConsulter(ABC):
-
-    @abstractmethod
-    def getMonsterByName(self, nombre:str) -> Monstruo:
-        conector = MHConnector()
-
-        pass
-
-    @abstractmethod
-    def getMonsterByDebilidad(self, debilidad:str) -> Monstruo:
-        pass
-
-    @abstractmethod
-    def getMonsterByResistencia(self, resistencia:str) -> Monstruo:
-        pass
-
+        
 class MHRecorder(ABC):
+
+    @abstractmethod
+    def setConnection(self, host:str, port:int) -> bool:
+        pass
 
     @abstractmethod
     def setMonstruo(self, monstruo:Monstruo) -> bool:
@@ -52,7 +30,6 @@ class MHRecorder(ABC):
     def checkIfExist(self, monstruo:Monstruo) -> bool:
         pass
 
-class MHWatcher(ABC):
     @abstractmethod
     def getSavedMonstruos(self) -> list:
         pass
