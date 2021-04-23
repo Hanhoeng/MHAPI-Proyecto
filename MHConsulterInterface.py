40a1c4d275cbeba2b6601af9312ad3d02b093b96
Hiroshi <sephirothMH@outlook.com>
2021-04-23 13:09:02 -0500

    main 100% funcional, integradas MHRecorder y MHConsulter al 100%

--- MHConsulterInterface.py
+++ MHConsulterInterface.py
@@ -10,12 +10,37 @@ class Monstruo():
         self.debilidades = debilidades
         self.resistencia = resistencia
         self.descripcion = descripcion
+        self.comentario = ""
+
+    def getNombre(self):
+        return self.nombre
+    def getDebilidad(self):
+        return self.debilidades
+    def getResistencia(self):
+        return self.resistencia
+    def getDescripcion(self):
+        return self.descripcion
+    def getComentario(self):
+        return self.comentario
+    def setComentario(self, comentario):
+        self.comentario = comentario
 
     def __str__(self) -> str:
-        self.cadena = "Nombre: {}\n".format(self.nombre)
-        self.cadena += "Debilidades: {}\n".format(self.debilidades)
-        self.cadena += "resistencia: {}\n".format(self.resistencia)
-        self.cadena += "descripción: {}\n".format(self.descripcion)
+        cadDebilidades = ""
+        cadResistencias = ""
+        for i in self.debilidades:
+            cadDebilidades+="\t{}\n".format(i)
+
+        for i in self.resistencia:
+            cadResistencias += "\t{}\n".format(i)
+
+        #print(cadDebilidades)
+
+        self.cadena = "Nombre:\n\t{}\n".format(self.nombre)
+        self.cadena += "Debilidades:\n{}".format(cadDebilidades)
+        self.cadena += "Resistencias:\n{}".format(cadResistencias)
+        self.cadena += "Descripción:\n\t{}\n".format(self.descripcion)
+        self.cadena += "Comentario:\n\t{}\n".format(self.comentario)
         return self.cadena
 
 # Interfaces
@@ -40,28 +65,44 @@ class MHConsulterBuscador(MHConsulter):
     col = db["Monster"]
 
     def getMonsterByName(self, nombre: str):
-        resultado = (self.col).find({},{"name":nombre})
-        name = resultado["name"]
+        resultado = self.col.find_one({"Nombre":nombre})
+        #print(type(resultado))
+        
+        nombre = resultado["Nombre"]
         descripcion = resultado["Descripcion"]
-        weakness = resultado["weakness"]
-        resistances = resultado["resistances"]
-        monstruo = Monstruo(name,weakness,resistances,descripcion)
+        debilidades = resultado["Debilidades"]
+        resistencias = resultado["Resistencias"]
+        monstruo = Monstruo(nombre,debilidades,resistencias,descripcion)
+        monstruo.setComentario(resultado["Comentario"])
         return monstruo
+        
 
     def getMonsterByDebilidad(self, debilidad: str):
-        resultado = (self.col).find({},{"weakness":debilidad})
-        name = resultado["name"]
-        descripcion = resultado["Descripcion"]
-        weakness = resultado["weakness"]
-        resistances = resultado["resistances"]
-        monstruo = Monstruo(name,weakness,resistances,descripcion)
-        return monstruo
+        monstruos=[]
+        resultado = self.col.find({"Debilidades":debilidad})
+
+        for i in resultado:
+            nombre = i["Nombre"]
+            debilidades = i["Debilidades"]
+            resistencias = i["Resistencias"]
+            descripcion = i["Descripcion"]
+            comentario = i["Comentario"]
+            monstruo = Monstruo(nombre,debilidades,resistencias,descripcion)
+            monstruo.setComentario(comentario)
+            monstruos.append(monstruo)
+        return monstruos
 
     def getMonsterByResistencia(self, resistencia: str):
-        resultado = (self.col).find({},{"resistances":resistencia})
-        name = resultado["name"]
-        descripcion = resultado["Descripcion"]
-        weakness = resultado["weakness"]
-        resistances = resultado["resistances"]
-        monstruo = Monstruo(name,weakness,resistances,descripcion)
-        return monstruo
+        resultado = self.col.find({"Resistencias":resistencia})
+        monstruos=[]
+
+        for i in resultado:
+            nombre = i["Nombre"]
+            debilidades = i["Debilidades"]
+            resistencias = i["Resistencias"]
+            descripcion = i["Descripcion"]
+            comentario = i["Comentario"]
+            monstruo = Monstruo(nombre,debilidades,resistencias,descripcion)
+            monstruo.setComentario(comentario)
+            monstruos.append(monstruo)
+        return monstruos
