fd725b0ab0a55a94d4e51f18a6cbf1626834c503
Hiroshi <sephirothMH@outlook.com>
2021-04-24 22:42:21 -0500

    Correccion de puntuacion en prints

--- main.py
+++ main.py
@@ -4,37 +4,37 @@ from pymongo.encryption import _MONGOCRYPTD_TIMEOUT_MS
 from MHRecorder import MHRecorderGuardador
 from MHConsulterInterface import MHConsulterBuscador
 
-print("Bienvenido a MHConsulter\n\nQue desea hacer?\n\n1.- Dame un monstruo al azar\n2.- Dame todos los monstruos encontrados\n3.- Buscar un monstruo")
+print("Bienvenido a MHConsulter.\n\nQue desea hacer?\n\n1.- Dame un monstruo al azar.\n2.- Dame todos los monstruos encontrados.\n3.- Buscar un monstruo.")
 eleccion0 = input()
 
 if eleccion0=="1":
     x = MHRecorderGuardador().getRandomMonster()
     print(x)
-    eleccion1 = input("Desea guardar el monstruo? (y/n)")
+    eleccion1 = input("Desea guardar el monstruo? (y/n): ")
 
     if eleccion1=="y":
         respuesta = MHRecorderGuardador().checkIfExist(x)
         if respuesta:
-            print("Este monstruo ya se encontro")
+            print("Este monstruo ya se encontro.")
         else:
-            print("Este monstruo aun no se encuentra")
-            eleccion2 = input("Desea registrarlo?(y/n)")
+            print("Este monstruo aun no se encuentra.")
+            eleccion2 = input("Desea registrarlo?(y/n): ")
             if eleccion2=="y":
-                comentario = input("Antes de guardar, agregue un comentario :D ")
+                comentario = input("Antes de guardar, agregue un comentario:  ")
                 x.setComentario(comentario)
                 if MHRecorderGuardador().setMonstruo(x):
-                    print("Se guardo con exito :D")
+                    print("Se guardo con exito!!!")
                 else:
-                    print("Algo fallo :c")
+                    print("Algo fallo!!!")
             else:
-                print("Weno :B chaito pues")
+                print("Hasta pronto.")
     else:
-        print("Weno :B Chaito pues")
+        print("Hasta pronto")
         pass
 elif eleccion0=="2":
     resultado = MHRecorderGuardador().getSavedMonstruos()
 elif eleccion0=="3":
-    eleccion4 = input("Por que quieres buscar...?\n1.- Por nombre\n2.- Por Debilidad\n3.- Por Resistencia\n")
+    eleccion4 = input("Por que quieres buscar...?\n1.- Por nombre.\n2.- Por Debilidad.\n3.- Por Resistencia.\n")
     if eleccion4=="1":
         nombreM = input("Dame el nombre: ")
         monstruo = MHConsulterBuscador().getMonsterByName(nombreM)
